ac7d7b782ccea754c8ccf65aa7ca9e1b731fce4f
tahakashaf <tahakashaf@gmail.com>
2016-05-04 19:10:39 -0700

    1st commit

---
+++ TDemo/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="TDemo.MvcApplication" Language="C#" %>
---
+++ TDemo/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(TDemo.Startup))]
+namespace TDemo
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
